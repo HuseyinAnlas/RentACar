e6b549b6e2a7d2072550aba4e45c5f2c2a019e6e
seres8seres8@hotmail.com <seres8seres8@hotmail.com>
2021-05-05 19:20:11 +0300

    Çalışma 8

---
+++ DataAccess/Concrete/Entity Framework/EfBrandDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Etities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Entity_Framework
+{
+    class EfBrandDal : IBrandDal
+    {
+        public void Add(Brand entity)
+        {
+            using (RentACarContext rentACarContext = new RentACarContext())
+            {
+                var addEntity = rentACarContext.Entry(entity);
+                addEntity.State = EntityState.Added;
+                rentACarContext.SaveChanges();
+
+            }
+        }
+
+        public void Delete(Brand entity)
+        {
+            using (RentACarContext rentACarContext = new RentACarContext())
+            {
+                var deletedEntity = rentACarContext.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                rentACarContext.SaveChanges();
+
+            }
+        }
+
+        public Brand Get(Expression<Func<Brand, bool>> filter)
+        {
+            using (RentACarContext rentACarContext = new RentACarContext())
+            {
+                return rentACarContext.Set<Brand>().SingleOrDefault(filter);
+
+            }
+        }
+
+        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
+        {
+            using (RentACarContext rentACarContext = new RentACarContext())
+            {
+                return filter == null ? rentACarContext.Set<Brand>().ToList() : rentACarContext.Set<Brand>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Brand entity)
+        {
+            using (RentACarContext rentACarContext = new RentACarContext())
+            {
+                var updatedEntity = rentACarContext.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                rentACarContext.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Etities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+
+    
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars =new List<Car>{
+                new Car{CarId=1, BrandId=1, ColorId=3, ModelYear=2021, DailyPrice=10000000, Description="Güzel"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car _carDelete = _cars.SingleOrDefault(p=>p.CarId==car.CarId);
+            _cars.Remove(_carDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(p => p.CarId == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p=>p.CarId==car.CarId);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
